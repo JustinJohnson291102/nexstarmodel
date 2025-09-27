325c3fb8ca60e7502150724e0542c5747b6d15f7
Justin Johnson <just4justin02@gmail.com>
2025-09-27 18:59:32 +0000

    increase the hero banner size more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { Button } from "@/components/ui/button";
@@ -31,7 +32,7 @@ export default function Home() {
   return (
     <>
       {/* ✅ FIX APPLIED: Hero banner height restored to a large size: 'md:h-[75vh]' */}
-      <section className="relative h-[70vh] md:h-[90vh] w-full"> 
+      <section className="relative h-screen w-full"> 
         <Image
           src="https://drive.google.com/uc?export=download&id=1AfTs2l2K095QR7q17gzL2eMtfRgIC3y4" // Using the corrected download URL
           alt="Hero banner showing a collage of business and technology images"
@@ -268,3 +269,5 @@ export default function Home() {
     </>
   );
 }
+
+    

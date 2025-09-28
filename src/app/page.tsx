8d753c263490381bc0c2cfa7f40c74412d78095d
Justin Johnson <just4justin02@gmail.com>
2025-09-28 15:31:12 +0000

    home page ke hero banner ka height thoda aur increase karo 10% more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
   return (
     <>
       <div className="w-full">
-        <section className="relative h-[112vh] w-full">
+        <section className="relative h-[122vh] w-full">
           <Carousel
             plugins={[
               Autoplay({
@@ -74,7 +74,7 @@ export default function Home() {
             <CarouselContent>
               {carouselImages.map((img, index) => (
                 <CarouselItem key={index}>
-                  <div className="relative h-[112vh] w-full">
+                  <div className="relative h-[122vh] w-full">
                     <Image
                       src={img.src}
                       alt={img.alt}
@@ -99,7 +99,7 @@ export default function Home() {
             <Button
               asChild
               size="lg"
-              className="mt-12 group transform scale-110"
+              className="mt-20 group transform scale-125"
             >
               <Link href="/services">
                 Our Services{" "}
@@ -316,5 +316,7 @@ export default function Home() {
     </>
   );
 }
+
+    
 
     

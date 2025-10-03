84c16e35924b52733ad4d10b2c210f7fe9b937c8
Justin Johnson <just4justin02@gmail.com>
2025-10-03 09:34:14 +0000

    set the height of the video same as the height of the hero container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -128,7 +128,7 @@ export default function Home() {
           <div className="absolute inset-0 bg-black/30" />
         </section>
         <div className="w-full md:w-[5%]" />
-        <div className="relative w-4/5 mx-auto md:w-[35%] h-[59vh] border border-gray-300 rounded-lg overflow-hidden my-4 md:my-0">
+        <div className="relative w-4/5 mx-auto md:w-[35%] h-[64vh] md:h-[91vh] border border-gray-300 rounded-lg overflow-hidden my-4 md:my-0">
           <video 
             src="https://ik.imagekit.io/ggelm1lwa/WhatsApp%20Video%202025-09-30%20at%2010.23.22%20PM.mp4?updatedAt=1759313283558"
             className="w-full h-full object-contain"

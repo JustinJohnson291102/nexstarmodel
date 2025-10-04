e47cdda926224a703598fc017924e5322eaaa018
Justin Johnson <just4justin02@gmail.com>
2025-10-04 08:55:13 +0000

    decrease the top height by 1.2%, bottom height by 1.2% and right width b

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,7 @@ export default function Home() {
                     />
                 </div>
             </div>
-            <div className="relative z-10 w-full h-full pl-[60%] pr-0 pt-0 pb-0">
+            <div className="relative z-10 w-full h-full pl-[60%] pr-[1.2%] pt-[1.2%] pb-[1.2%]">
               <video 
                 src="https://ik.imagekit.io/ggelm1lwa/hero%20banner%20video.mp4?updatedAt=1759563722583"
                 className="w-full h-full object-cover"

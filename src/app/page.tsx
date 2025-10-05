65aba43faa69070d74201a1ba138e224615bd204
Justin Johnson <just4justin02@gmail.com>
2025-10-05 17:24:16 +0000

    below the we offer in home page we have animated texts container which h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
               muted
               playsInline
             />
-            <div className="relative z-10 flex flex-col items-start justify-center p-4 w-full h-full ml-[60px] pl-[5px]">
+            <div className="relative z-10 flex flex-col items-start justify-center p-4 w-full h-full ml-[60px] pl-[15px]">
                  <AnimatedText />
             </div>
           </section>

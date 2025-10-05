c768073bab5e7d1021e5989b62ddbcf4f38c7f18
Justin Johnson <just4justin02@gmail.com>
2025-10-05 17:20:03 +0000

    increase the margin-left by 10 px and shift it to right by 5 px and plea

--- src/components/homepage/animated-text.tsx
+++ src/components/homepage/animated-text.tsx
@@ -9,10 +9,10 @@ export default function AnimatedText() {
     <div className="animated-text-container">
       <span className="prefix mb-4 md:mb-12">We offer</span>
       <div className="message">
-        <div className="word1">Web Development</div>
-        <div className="word2">Digital Marketing</div>
-        <div className="word3">B2B Marketing</div>
-        <div className="word4">Augmented Reality</div>
+        <div className="word1 word-animation">Web Development</div>
+        <div className="word2 word-animation">Digital Marketing</div>
+        <div className="word3 word-animation">B2B Marketing</div>
+        <div className="word4 word-animation">Augmented Reality</div>
       </div>
     </div>
   );
